0ea6b0b7595b0f4364463424d6bbc6e2a4ba3e94
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2023-08-12 22:01:06 +0530

    feat: add tutor-signup

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('SailingPen API')
+    .setDescription('SailingPen API description')
+    .setVersion('1.0')
+    .addTag('sailingpen')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
