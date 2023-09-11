75cae9465414344c30a995530599a745191f1be6
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2023-09-11 22:16:57 +0530

    feat: add library and library folder models and routes to create and fetch folders

---
+++ src/library/library.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Body, Post, UseGuards, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { LibraryService } from './library.service';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { AuthenticatedUser } from 'src/auth/types/jwt.types';
+import { CreateLibraryFolderDto } from './dto/create-library-folder.dto';
+
+@Controller('library')
+export class LibraryController {
+  constructor(private readonly libraryService: LibraryService) {}
+
+  @Roles('tutor')
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @Get('folders')
+  getFolderStructure(@Req() req: Request) {
+    const user = req.user as AuthenticatedUser;
+    return this.libraryService.getFoldersForTutor(user.sub);
+  }
+
+  @Roles('tutor')
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @Post('folders')
+  createFolder(
+    @Req() req: Request,
+    @Body() createLibraryFolderDto: CreateLibraryFolderDto,
+  ) {
+    const user = req.user as AuthenticatedUser;
+    return this.libraryService.createFolder(user.sub, createLibraryFolderDto);
+  }
+}
---
+++ src/types/library.types.ts
@@ -0,0 +1,5 @@
+export interface LibraryFolder {
+  id: number;
+  name: string;
+  children: LibraryFolder[];
+}
