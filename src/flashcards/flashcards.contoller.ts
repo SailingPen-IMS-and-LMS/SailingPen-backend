3ac9d554c1fcd46177f9ec3cd92400eada90b988
Avishka Sathyanjana <baavishka@gmail.com>
2023-10-28 17:42:51 +0530

    fix: issue in prisma

--- src/flashcards/flashcards.contoller.ts
+++ src/flashcards/flashcards.contoller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get, Post, Body, UseGuards, HttpCode, HttpStatus, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+  Req,
+} from '@nestjs/common';
 import { FlashcardsService } from './flashcards.service';
 import { CreateFlashcardDeckDto } from './dto/create-flashcard.dto';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/common/guards/roles.guard';
-import {JwtAuthGuard} from "../common/guards/jwt-auth.guard";
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import {AuthenticatedUser} from "../auth/types/jwt.types";
+import type {Request} from 'express'
 
 @Controller('flashcards')
 export class FlashcardsController {
@@ -11,20 +22,25 @@ export class FlashcardsController {
 
   @Get('')
   @Roles('tutor')
-  @UseGuards(JwtAuthGuard, RolesGuard )
+  @UseGuards(JwtAuthGuard, RolesGuard)
   getAllFlashcards() {
     return this.flashcardsService.getAllFlashcards();
   }
 
-
-  //to create a flashcard deck 
-  @Post()
+  //to create a flashcard deck
+  @Post('')
   @Roles('tutor')
-  @UseGuards(JwtAuthGuard, RolesGuard )
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @HttpCode(HttpStatus.CREATED)
   createFlashcard(
-    @Req() req: RequestDestination,
-    @Body() createFlashcardDeckDto: CreateFlashcardDeckDto) {
-    return this.flashcardsService.createFlashcardDeck(createFlashcardDeckDto);
+    @Req() req: Request,
+    @Body() createFlashcardDeckDto: CreateFlashcardDeckDto,
+  ) {
+    const user = req.user as AuthenticatedUser;
+    const userId = user.sub;
+    return this.flashcardsService.createFlashcardDeck(userId, createFlashcardDeckDto);
   }
+
+
+  
 }
