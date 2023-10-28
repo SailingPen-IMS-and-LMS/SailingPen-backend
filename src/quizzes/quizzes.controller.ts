b6efa64b316e96642d695e0b973b4f20031e7601
amanividanage <amanividanage@gmail.com>
2023-10-28 17:38:55 +0530

    feat: tutor quiz

--- src/quizzes/quizzes.controller.ts
+++ src/quizzes/quizzes.controller.ts
@@ -1,21 +1,50 @@
-import { Controller, Get ,Body, Post} from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Param, UseGuards, Req } from '@nestjs/common';
 import { QuizzesService } from './quizzes.service';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Request } from 'express';
+import { CreateQuizDto } from './dto/create-quiz.dto';
+import { AuthenticatedUser } from 'src/auth/types/jwt.types';
+
+
 
 @Controller('quizzes')
 export class QuizzesController {
   constructor(private readonly quizzesService: QuizzesService) {}
 
+  @Get('/:quizId')
+  getQuiz(@Param('quizId') quizId: string) {
+    return this.quizzesService.getQuiz(quizId);
+  }
 
 
 
-    @Get("/")
-    getAllQuiz() {
-        return this.quizzesService.getAllQuiz()
-    }
-    @Post("/create")
-    createQuiz(@Body() quizData: any) {
-        return {data: quizData};
-    }
 
- 
+
+@Roles('tutor')
+@UseGuards(JwtAuthGuard, RolesGuard)
+  @Post('')
+  createQuiz( @Req() req:Request,@Body() quizData: CreateQuizDto) {
+
+
+    const user= req.user as AuthenticatedUser;
+    const userId=user.sub;
+    return this.quizzesService.createQuiz(userId,quizData);
+  }
+
+  @Post('/:quizId/questions')
+  createQuestion(@Param('quizId') quizId: string, @Body() questionData: any) {
+    return this.quizzesService.createQuestion(quizId, questionData);
+  }
+
+  @Put('/:quizId/publish')
+  publishQuiz(@Param('quizId') quizId: string) {
+    return this.quizzesService.publishQuiz(quizId);
+  }
 }
+
+
+
+
+
--- src/quizzes/quizzes.service.ts
+++ src/quizzes/quizzes.service.ts
@@ -1,12 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { CreateQuizDto, CreateQuestionDto } from './dto/create-quiz.dto';
 
 @Injectable()
 export class QuizzesService {
+  constructor(private readonly prisma: PrismaService) {}
 
+  async createQuiz(userId: string, quizData: CreateQuizDto) {
+    // Check if the tutor ID is valid
 
-    getAllQuiz() {
-        return "Hello from QuizzesService"
+    const { description, title, published } = quizData;
+    // Create a new quiz in the database
+    const createdQuiz = await this.prisma.quiz.create({
+      data: {
+        title,
+        published,
+        description,
+        tutor: { connect: { user_id: userId } },
+      },
+    });
+
+    return createdQuiz;
+  }
+
+  async createQuestion(quizId: string, questionData: CreateQuestionDto) {
+    // Check if the quiz exists
+    const quiz = await this.prisma.quiz.findUnique({
+      where: { quiz_id: quizId },
+    });
+    if (!quiz) {
+      throw new NotFoundException('Quiz not found');
     }
 
+    // Create a new question associated with the quiz
+    const createdQuestion = await this.prisma.question.create({
+      data: {
+        ...questionData,
+        quiz: { connect: { quiz_id: quizId } },
+      },
+    });
+    return createdQuestion;
+  }
 
+  async publishQuiz(quizId: string) {
+    // Update the quiz to mark it as published
+    const publishedQuiz = await this.prisma.quiz.update({
+      where: { quiz_id: quizId },
+      data: { published: true },
+    });
+    return publishedQuiz;
+  }
+
+  async getQuiz(quizId: string) {
+    // Retrieve quiz details, including its questions and answers
+    const quiz = await this.prisma.quiz.findUnique({
+      where: { quiz_id: quizId },
+      include: {
+        questions: {
+          include: {
+            answers: true,
+          },
+        },
+      },
+    });
+
+    if (!quiz) {
+      throw new NotFoundException('Quiz not found');
+    }
+
+    return quiz;
+  }
 }
