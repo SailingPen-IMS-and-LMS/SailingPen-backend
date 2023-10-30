010be115ac2ef177443c1733ab7332b86ebadf9d
Avishka Sathyanjana <baavishka@gmail.com>
2023-10-30 20:37:38 +0530

    feat: add updateAnnouncement function

--- src/announcements/announcements.service.ts
+++ src/announcements/announcements.service.ts
@@ -4,7 +4,10 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { CreateAnnouncementDto } from './dto/create-announcements.dto';
+import { 
+  CreateAnnouncementDto, 
+  UpdateAnnouncementDto, 
+} from './dto/create-announcements.dto';
 
 @Injectable()
 export class AnnouncementsService {
@@ -52,6 +55,41 @@ export class AnnouncementsService {
       });
     
       return announcement;
+    }
+
+    //to update announcements
+    async updateAnnouncement(
+      userId: string,
+      announcementId: number,
+      updateAnnouncementDto: UpdateAnnouncementDto,
+    ) {
+      const { title, content } = updateAnnouncementDto;
+    
+      const tutor = await this.prisma.tutor.findUnique({
+        where: { user_id: userId },
+      });
+    
+      if (!tutor) {
+        throw new NotFoundException(`Tutor with User ID ${userId} not found`);
+      }
+    
+      const announcement = await this.prisma.announcement.findUnique({
+        where: { id: announcementId },
+      });
+    
+      if (!announcement || announcement.tutor_id !== tutor.tutor_id) {
+        throw new NotFoundException(`Announcement with ID ${announcementId} not found or not owned by Tutor with User ID ${userId}`);
+      }
+    
+      const updatedAnnouncement = await this.prisma.announcement.update({
+        where: { id: announcementId },
+        data: {
+          title: title ?? announcement.title,
+          content: content ?? announcement.content,
+        },
+      });
+    
+      return updatedAnnouncement;
     }
 
 }
--- src/announcements/dto/create-announcements.dto.ts
+++ src/announcements/dto/create-announcements.dto.ts
@@ -1,16 +1,36 @@
-import { IsString, IsNotEmpty, IsArray, ValidateNested } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsArray,
+  ValidateNested,
+  IsOptional,
+} from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateAnnouncementDto {
-    @IsNotEmpty()
-    @IsString()
-    title: string;
-  
-    @IsNotEmpty()
-    @IsString()
-    content: string;
-  
-    @IsNotEmpty()
-    @IsString()
-    tution_class_id: string;
-  }
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @IsNotEmpty()
+  @IsString()
+  tution_class_id: string;
+}
+
+export class UpdateAnnouncementDto {
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  content?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  tution_class_id: string;
+}
