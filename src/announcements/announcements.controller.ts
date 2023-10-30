d2d877485aeb2bf3be8aa520d45136bf44907882
Avishka Sathyanjana <baavishka@gmail.com>
2023-10-30 10:54:20 +0530

    feat: create initail codes for announcemet controller

---
+++ src/announcements/announcements.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { AnnouncementsService } from './announcements.service';
+import { CreateAnnouncementDto } from './dto/create-announcement.dto';
+import { UpdateAnnouncementDto } from './dto/update-announcement.dto';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { AuthenticatedUser } from '../auth/types/jwt.types';
+import type { Request } from 'express';
+
+
+@Controller('announcements')
+export class AnnouncementsController {
+  constructor(private readonly announcementsService: AnnouncementsService) {}
+
+  @Post()
+  create(@Body() createAnnouncementDto: CreateAnnouncementDto) {
+    return this.announcementsService.create(createAnnouncementDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.announcementsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.announcementsService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateAnnouncementDto: UpdateAnnouncementDto,
+  ) {
+    return this.announcementsService.update(+id, updateAnnouncementDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.announcementsService.remove(+id);
+  }
+}
